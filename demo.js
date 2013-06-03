ad85713602d43783a05a3fee1fd9b7c2da22fd3c
Robert Hartung <r.hartung@roberthartung.de>
2013-06-03 17:56:30 +0200

    Demo enhanced

--- demo.js
+++ demo.js
@@ -19,17 +19,8 @@ live.on('connect', function()
 live.on('status', function(client)
 {
 	//console.log('client:', client);
-	console.log('received event "status" from client, args:', arguments);
+	console.log('received event "status" from client');
 	client.send('Status received');
 });
 
 console.log('Running...');
-
-/*
-var live2 = require('livejs')(http.createServer().listen(1338));
-
-live2.on('event', function()
-{
-	console.log('received event "event"');
-});
-*/
